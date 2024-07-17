aba67e2180033e627133740f648e95744f793aff
Dulshan The Developer <abavira@gmail.com>
2024-07-18 00:47:48 +0530

    pusher setup

--- src/Utils/pusher.js
+++ src/Utils/pusher.js
@@ -2,13 +2,7 @@ import PusherJS from 'pusher-js'
 
 PusherJS.logToConsole = true;
 
-let pusher = new PusherJS( import.meta.env.VITE_PUSHER_APP_KEY, {
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    forceTLS: false,
-    encrypted: true,
-    disableStats: true,
-    enabledTransports: ['ws', 'wss'],
+let pusher = new PusherJS( import.meta.env.VITE_PUSHER_APP_KEY, {   
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 });
 
